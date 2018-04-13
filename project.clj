26b7be854b5c6c0cfd078cbee6635f143ba1d970
Noah Kennedy <Nomaxx117@gmail.com>
2018-04-13 13:41:38 -0500

    Implemented new server code

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [venantius/pyro "0.1.2"]]
   :main ^:skip-aot clojureserv.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojureserv/core.clj
+++ src/clojureserv/core.clj
@@ -1,9 +1,9 @@
 (ns clojureserv.core
-  (:import (java.net Socket InetAddress)
+  (:import (java.net Socket InetAddress ServerSocket)
            (java.io PrintWriter)
            (java.util Scanner)))
 
-(defn ^Socket mk-tcp-socket [^String address,
+(defn ^Socket mk-client-socket [^String address,
                              ^Integer port]
   (Socket. (InetAddress/getByName address) port))
 
@@ -22,3 +22,14 @@
   (send-off socket-agent #(-> %
                               mk-tcp-writer
                               (.println str))))
+
+(defn ^ServerSocket mk-server-socket [^Integer port]
+  (ServerSocket. port))
+
+(def server
+  (-> (mk-server-socket 2401)
+      agent))
+
+(def client
+  (-> (mk-client-socket "127.0.0.1" 2401)
+      agent))
